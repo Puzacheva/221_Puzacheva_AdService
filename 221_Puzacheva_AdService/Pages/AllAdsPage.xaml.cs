5b591880f335e1c0a53a937d9728213caa9f08ef
Puzacheva <olga.isfff@gmail.com>
2025-01-25 06:06:19 +0300

    Добавьте файлы проекта.

---
+++ 221_Puzacheva_AdService/Pages/AllAdsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _221_Puzacheva_AdService.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AllAdsPage.xaml
+    /// </summary>
+    public partial class AllAdsPage : Page
+    {
+        public AllAdsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
